df014da316afff4a17a14ebaa47d44ef6608d3bb
Karthik S <92262232+skarthik05@users.noreply.github.com>
2025-02-23 17:36:55 +0530

    chore: setup eslint, prettier, and swc configuration (#1)
    * chore(deps): eslint, swc and add lint scripts
    
    * chore(eslint): create rules and config
    
    * chore(swrc): initialize SWRC
    
    * chore(prettier): add .prettierrc rules and .prettierignore file
    
    * chore(tsconfig): update target, enable strict null checks, and enforce noUnusedLocals
    
    * fix(e2e): resolve "any" type issues by adding type cast for "app"
    
    * chore: update .gitignore and README.md

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { type INestApplication } from '@nestjs/common';
+import { Test, type TestingModule } from '@nestjs/testing';
+import { type Server } from 'http';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+
+import { AppModule } from '../src/app.module';
+import { AppService } from '../src/app.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', () =>
+    request(app.getHttpServer() as unknown as Server)
       .get('/')
       .expect(200)
-      .expect('Hello World!');
-  });
+      .expect('Hello World!'));
 });

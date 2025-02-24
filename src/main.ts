6bfb7ab3f24d858f10793262e5ec2bc1e41d78e5
karthik <karthiks0598@gmail.com>
2025-02-24 15:41:33 +0530

    chore(docs): setup Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { type NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import { setupSwagger } from './setup-swagger';
+import { ApiConfigService } from './shared/services/api-config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ApiConfigService);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix(configService.apiPrefix, {
+    exclude: ['/'],
+  });
+  if (configService.documentationEnabled) {
+    setupSwagger(app);
+  }
+  if (configService.isNotDevelopment) {
+    app.enableShutdownHooks();
+  }
+  await app.listen(configService.port);
 }
 
 void bootstrap();

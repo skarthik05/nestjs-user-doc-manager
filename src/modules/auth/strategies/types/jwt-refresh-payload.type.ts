0dff5efcc1ed950aceaecadad9178fbabb81e13a
karthik <karthiks0598@gmail.com>
2025-03-01 14:25:00 +0530

    feat(types): add JWT access token, and refresh token payload type

---
+++ src/modules/auth/strategies/types/jwt-refresh-payload.type.ts
@@ -0,0 +1,8 @@
+import { type Session } from '../../../session/domain/session';
+
+export type JwtRefreshPayloadType = {
+  sessionId: Session['id'];
+  hash: Session['hash'];
+  iat: number;
+  exp: number;
+};

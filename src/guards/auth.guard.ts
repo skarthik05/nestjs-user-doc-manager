767cbae89ca4e9737daf364380643b446429a27e
karthik <karthiks0598@gmail.com>
2025-03-02 00:58:30 +0530

    feat(guard): combine auth & role guard for better management

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,58 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+
+import { RoleType } from '../common/types/role.type';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { JwtPayloadType } from '../modules/auth/strategies/types/jwt-payload.type';
+
+interface RequestWithUser extends Request {
+  user: JwtPayloadType;
+}
+
+@Injectable()
+export class JwtRolesGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<RoleType[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+
+    if (!this.isValidJwtPayload(user)) {
+      return false;
+    }
+
+    return requiredRoles.some((role) => Number(user.roleId) === Number(role));
+  }
+
+  handleRequest<TUser = JwtPayloadType>(
+    err: Error,
+    user: TUser | false,
+  ): TUser {
+    if (err || !user) {
+      throw (
+        err || new UnauthorizedException('Invalid token or no token provided')
+      );
+    }
+    return user;
+  }
+
+  private isValidJwtPayload(user: JwtPayloadType): user is JwtPayloadType {
+    return user && typeof user.roleId === 'number';
+  }
+}
